6467d6b5f575186ca383c4ac9c6368c7d434ad2c
Sohail <sohailraj.satapathy@gmail.com>
2025-06-04 09:28:18 +0530

    Final working build

--- install/serial_mpu_reader/share/serial_mpu_reader/launch/imu_visualize.launch.py
+++ install/serial_mpu_reader/share/serial_mpu_reader/launch/imu_visualize.launch.py
@@ -3,6 +3,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
+from launch.conditions import IfCondition
 
 def generate_launch_description():
     use_rviz = LaunchConfiguration('rviz')
